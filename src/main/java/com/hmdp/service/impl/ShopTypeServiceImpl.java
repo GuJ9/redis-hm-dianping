6700ceb167b66de0382e3d36e9940383dde37f7e
GuJ9 <2956391157@qq.com>
2026-06-30 22:16:29 +0800

    商户查询缓存功能

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -16,5 +29,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
-
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Override
+    public Result queryTypelist() {
+        //1.查询redis
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+        //2.redis里面有数据就直接返回
+        if (StrUtil.isNotBlank(shopTypeJson)){
+            List<ShopType> typeList = JSONUtil.toList(shopTypeJson, ShopType.class);
+            return Result.ok(typeList);
+        }
+        //3.redis里面没有数据，查询数据库
+        List<ShopType> typeList = query().
+                orderByAsc("sort").list();
+        //4.数据库有没有就返回空集合，避免前端报错
+        if (typeList == null){
+            typeList= Collections.emptyList();
+        }
+        //5.写入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY,
+                JSONUtil.toJsonStr(typeList),1, TimeUnit.DAYS);
+        //6.返回结果
+        return Result.ok(typeList);
+    }
 }
